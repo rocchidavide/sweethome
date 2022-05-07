d0d70edff83b66912ac224612ac1bf62fa15ae12
Davide Rocchi <rocchi.davide@gmail.com>
2022-05-07 10:46:07 +0200

    Add packages into install.sh Reduce installed dots by default

--- install.sh
+++ install.sh
@@ -5,16 +5,21 @@ function printHeader() {
 }
 
 
-printHeader "Install APT packages"
+printHeader "APT: update sources"
 apt update
+printHeader "APT: install packages"
 apt install --yes \
-  tilix \
-  flameshot peek \
-  megasync \
-  fonts-firacode fonts-powerline ttf-mscorefonts-installer
-  # python3-dev python3-pip python3-venv python3-setuptools python3-wheel \
-  # git \
-  # build-essential \
+    sshfs \
+    htop \
+    tilix \
+    flameshot peek \
+    fonts-firacode fonts-powerline ttf-mscorefonts-installer \
+    rofi \
+    python3.8-venv python3-pyqt5 \
+    # megasync \
+    # python3-dev python3-pip python3-venv python3-setuptools python3-wheel \
+    # git \
+    # build-essential \
 
 if ! command -v starship &> /dev/null
 then
@@ -23,3 +28,5 @@ then
 else
   printHeader "Starship already installed"
 fi
+
+# ~/.sweethome/dotmanage.py install --force
